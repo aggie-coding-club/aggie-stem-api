234a263c52103bec349e767dc8641e8347d13ddc
Thomas McDonald <thomasmcdonald@Thomass-MacBook-Pro.local>
2018-05-09 17:23:08 -0500

    updated port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 9000,
     domain = process.env.DOMAIN || 'localhost';
     mongoose = require('mongoose');
     Task = require('./api/models/Model'),
